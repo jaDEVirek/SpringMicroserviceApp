3fbc65b0e6dcf092550b3bec4ac799b2052c444e
Wiktor Religo <wiciorny@gmail.com>
2023-01-15 20:19:13 +0100

    adding load balancer

--- medicaltest-service/src/main/java/com/jadevirek/medicaltestservice/web/MedicalTestController.java
+++ medicaltest-service/src/main/java/com/jadevirek/medicaltestservice/web/MedicalTestController.java
@@ -5,8 +5,11 @@ import com.jadevirek.medicaltestservice.domain.entities.MedicalTest;
 import com.jadevirek.medicaltestservice.service.MedicalTestService;
 import com.jadevirek.medicaltestservice.utils.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -15,15 +18,31 @@ import java.util.List;
 @RequestMapping("/medicaltest")
 public class MedicalTestController {
     private final MedicalTestService medicalTestService;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public MedicalTestController(MedicalTestService medicalTestService) {
+    public MedicalTestController(MedicalTestService medicalTestService, RestTemplate restTemplate) {
         this.medicalTestService = medicalTestService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping
     public List<MedicalTest> getAllMedicalTests() {
-        return medicalTestService.getAllMedicalTests();
+          return  this.medicalTestService.getAllMedicalTests();
+    }
+
+    @GetMapping("patients")
+    public List<MedicalTest> getAllMedicalTestsForPatients() {
+        return getAllPatientsMedicalTests();
+    }
+    public List<MedicalTest> getAllPatientsMedicalTests() {
+        // Example of using Ribbon Load Balancer.
+        // It can be received from another services.
+        final ResponseEntity<List<MedicalTest>> medicalTestResponse = this.restTemplate.exchange(
+                "http://PATIENTSERVICE/patient/tests",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<MedicalTest>>() {
+                });
+        return medicalTestResponse.getBody();
     }
 
     @GetMapping("/{id}")
